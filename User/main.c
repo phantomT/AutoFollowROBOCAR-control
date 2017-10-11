01eb9c8da5c8e5f14872695022e9c098071ee0c0
phantomT <bobphantom@126.com>
2017-10-11 17:49:58 +0800

    增加了串口回传实验代码
    增加了串口回传部分代码，可以实现回传发送的字符串
    同时保留了上一版本的PWM波输出

--- User/main.c
+++ User/main.c
@@ -2,14 +2,15 @@
   ******************************************************************************
   * @file    main.c
   * @author  FireIce
-  * @version V1.0
-  * @date    2017-10-10
-  * @brief   通用定时器PWM输出
+  * @version V1.1
+  * @date    2017-10-11
+  * @brief   通用定时器PWM输出+串口
   ******************************************************************************
   */
   
-#include "stm32f4xx.h"
+#include <stm32f4xx.h>
 #include "./tim/bsp_general_tim.h"
+#include "./usart/bsp_debug_usart.h"
 
 /**
   * @brief  主函数
@@ -21,6 +22,14 @@ int main(void)
   /* 初始化通用定时器PWM输出 */
 	TIMx_Configuration01(20,40,60,80);				//PA0-PA3口的PWM波输出占空比
   TIMx_Configuration02(30,50,70,90);				//PH9-PH12口的PWM波输出占空比
+	
+	/*初始化USART 配置模式为 115200 8-N-1，中断接收*/
+  Debug_USART_Config();
+	
+	/* 发送一个字符串 */
+	Usart_SendString( DEBUG_USART,"这是一个串口中断接收回显实验\n");
+	printf("这是一个串口中断接收回显实验\n");
+	
   while(1)
   {       
   }
